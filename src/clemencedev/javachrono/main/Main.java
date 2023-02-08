d648bc46e46dd553f5bcfa47db6fe92e9b2eefa3
shobaze <122310973+clemencedev@users.noreply.github.com>
2023-02-08 17:56:26 +0100

    Added main class

---
+++ src/clemencedev/javachrono/main/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author clemencedev
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+package clemencedev.javachrono.main;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
